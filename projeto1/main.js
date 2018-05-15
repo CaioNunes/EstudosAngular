37d054adddfc0dffec8336cb6b8d6aa1096770c7
CaioNunes <caiofelipe147@gmail.com>
2018-05-15 18:39:17 -0300

    Alteração para atividade 2

--- projeto1/main.js
+++ projeto1/main.js
@@ -1,4 +1,20 @@
 var app = angular.module('Projeto1',[]);
 app.controller('FirstController', function($scope){
     $scope.mensagem = "Olá , esse é meu primeiro projeto em Angular !"
+
 });
+
+app.controller('MinhaController', ['$scope',function($scope){
+    $scope.mensagem = "Meu Segundo Projeto"
+
+    $scope.verificaValor = function(valor){
+        if(valor > 0){
+            $scope.mensagem = 'O número digitado é positivo';
+        }else if(valor < 0){
+            $scope.mensagem = 'O número digitado é negativo';
+        }else{
+            $scope.mensagem = 'Você digitou zero!';
+        }
+    };
+
+}]);
